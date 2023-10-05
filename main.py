4d8db3567da342528cf2ef9262cea87663b26a6e
El_Asho <Nashpotatoes2008@gmail.com>
2023-10-05 09:54:49 -0500

    new stuff

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from kivy.lang import Builder
 
 Builder.load_file("Quiz-Page.kv")
 
-
+users = ["dog21","YellowisMellow","Bowser35","TikiTorch10"]
+passwords = {"dog21":"Dallas76","YellowisMellow":"George98","Bowser35":"Apple124","TikiTorch10":"AXBYndRgyY5"}
 
 class QuizPageApp(App):
     def build(self):
@@ -33,13 +34,27 @@ class Question2Screen(Screen):
    pass
 class CorrectScreen(Screen):
     def correct_advance(self):
-        self.manager.current = "question2"
+        self.manager.current = "login"
 
 
 
 class ErrorScreen(Screen):
     def error_advance(self):
         self.manager.current = "question2"
+class LoginPage(Screen):
+    def check_user(self,username,password):
+        if username in users and passwords[username] == password:
+           self.manager.current = "welcome"
+        else:
+           blank.text = "No User or Password does not match"
+
+
+
+
+class NewUser(Screen):
+    pass
+class WelcomeScreen(Screen):
+    pass
 
 
 
